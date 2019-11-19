b0e0128a3637c6e8eaa40525cd21778610bfb294
Himanshu Sharma <himanshush214@gmail.com>
2019-11-19 10:40:22 +0530

    My first java program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    int a = 5;
+    int b = 15;
+    String name = "Himashu Sharma";
+    System.out.println("Hello world by "+name+" and age of "+name+" is "+(a+b));
+  }
+}
